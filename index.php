bfb671c9c0546c62ab6c05d11c689d5c4a12ce83
Gabriel Abdalla <gabriel.abdalla@gmail.com>
2021-07-25 23:07:16 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
+/*
 $root = new Usuario();
-
 $root->loadById(4);
-
 echo $root;
+*/
+
+// Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "roott");
+
+echo $usuario;
 
 ?>
