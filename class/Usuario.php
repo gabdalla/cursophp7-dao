74a9e0c0ee2c74feb1d0cddda7b6ddd120057a87
Gabriel Abdalla <gabriel.abdalla@gmail.com>
2021-07-25 23:44:13 -0300

    comando delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -133,6 +133,20 @@ class Usuario
 		));
 	}
 	
+	public function delete()
+	{
+		$sql = new Sql();
+		
+		$results = $sql->select("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID' => $this->getIdusuario()
+		));
+		
+		$this->setIdusuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+	}
+	
 	public function __tostring()
 	{
 		return json_encode(array(
--- index.php
+++ index.php
@@ -28,9 +28,17 @@ $aluno->insert();
 echo $aluno;
 */
 
+/*
+// Alterar um usuário
 $usuario = new Usuario();
 $usuario->loadById(8);
 $usuario->update("professor", "!@#$%¨&*");
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+$usuario->loadById(7);
+$usuario->delete();
 echo $usuario;
 
 ?>
